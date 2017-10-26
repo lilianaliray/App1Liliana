c3ce02b18757ce92d85acebfe51244596614a1f7
lilianaliray <marcelo_masoka@hotmail.com>
2017-10-25 21:07:50 -0300

    resultados de proyecto suma, resta,divicion y multiplicacion

--- src/app1liliana/App1Liliana.java
+++ src/app1liliana/App1Liliana.java
@@ -9,13 +9,51 @@ package app1liliana;
  *
  * @author LAB04
  */
-public class App1Liliana {
-
+public class App1Liliana
+{
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    public static void main(String[] args) 
     
-}
+    {
+       int numero;
+       int numero2;
+       String name;
+       
+       
+       numero=10;
+       numero2=40;
+       name="Lili";
+       
+       
+       System.out.println("Suma de numero y numero2:;"+(numero+numero2));
+        System.out.println("Bienvenido\t"+name);
+       System.out.println("Resta de numero2 y numero:;"+(numero2-numero));
+       System.out.println("Bienvenido\t"+name);
+       System.out.println("Divicion de numero2 y numero:;"+(numero2/numero));
+       System.out.println("Bienvenido\t"+name);
+       System.out.println("Multiplicacion de numero2 y numero:;"+(numero2*numero));
+       System.out.println("Residuo de numero y numero2:;"+(numero%numero2));
+       System.out.println("Bienvenido\t"+name);
+       
+      
+      }
+    
+        
+    }
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+        // TODO code application logic here
+    
+    
+
